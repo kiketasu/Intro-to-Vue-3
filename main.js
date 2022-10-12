64eb41749dcf2da48a21bb194af6773617d2cd56
Enric <enricorillosa@hotmail.es>
2022-10-12 10:43:02 +0200

    done 8

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 const app = Vue.createApp({
     data() {
         return {
+            onSale: true,
+            selectedVariant: 0,
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
-              ],
-              sizes: ['S', 'M', 'L', 'XL']
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
         }
     },
     methods: {
         addToCart(){
             this.cart+=1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            
         },
-        removeCart(){
-            this.cart = 0
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        saleMessage() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
         }
         
     }
