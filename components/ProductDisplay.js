2d9b8ac11cf9a5ed5ec0d52a7e16d19ffa7cde42
Enric <enricorillosa@hotmail.es>
2022-10-12 11:57:28 +0200

    done 10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -45,28 +45,28 @@ app.component('product-display', {
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
         ]
     }
-},
-methods: {
-    addToCart() {
-        this.cart += 1
-    },
-    updateVariant(index) {
-        this.selectedVariant = index
-    }
-},
-computed: {
-    title() {
-        return this.brand + ' ' + this.product
-    },
-    image() {
-        return this.variants[this.selectedVariant].image
-    },
-    inStock() {
-        return this.variants[this.selectedVariant].quantity
-    },
-    shipping(){
-        if(this.premium){
-            return 'Free'
+  },
+  methods: {
+      addToCart() {
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      }
+  },
+  computed: {
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
         }
         return 2.99
     }
--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data() {
-      return {
-        cart: 0,
-        premium: true
-      }
+        return {
+            cart: [],
+            premium: true
+        }
     },
-    methods: {}
-  })
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        }
+    }
+})
